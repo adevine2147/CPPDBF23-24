85d70ee962ec2d723fd09de2058acad9b5ad41bc
Charles Ou <charlesou@b98-aruba-authenticated-10-110-151-73.wlan.cpp.edu>
2023-10-03 11:29:37 -0700

    fixing deleteing old week summaries

--- summaries.py
+++ summaries.py
@@ -1,90 +1,84 @@
 import os
 import re
 from github import Github
+from github import Auth
 import sys
 
-
-repository_name = 'CPPDBF23-24'
+repository_name = 'adevine2147/CPPDBF23-24'
 
 # Define the GitHub token secret name
 github_token_secret_name = 'GH_PAT'
 
 # Get the GitHub token from the secret
 #github_token = os.getenv(github_token_secret_name)
-github_token = "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"
 
-# Check if the token is available
-if github_token is None:
-    sys.exit(f"GitHub token '{github_token_secret_name}' not found in environment variables.")
+# Authentication is defined via github.Auth
+auth = Auth.Token("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")
+
+# Public Web Github
+g = Github(auth=auth)
 
 # Initialize the GitHub API client with the token
-g = Github(github_token)
-
-repo = g.get_user().get_repo(repository_name)
+repo = g.get_repo(repository_name)
+print(repo)
 
 # Define the folder containing weekly update markdown files
 folder_path = 'systems/updates'
 
-# List all directories in the folder
-directories = [directory for directory in repo.get_contents(folder_path, ref="main") if directory.type == 'dir']
+contents = repo.get_contents(folder_path, ref="main")
+
+# Iterate through the contents to find directories
+directories = [content for content in contents if content.type == 'dir']
+
 # Initialize a dictionary to store summaries by week number
 summaries_by_week = {}
 
 # Initialize a variable to store the combined summary for the main readme
 combined_main_readme_summary = ""
+latest_week_number = 0  # Initialize with a default value
+latest_week_summary = ""  # Initialize with an empty string
 
-# Iterate through each directory
 for directory in directories:
     # List all markdown files in the subdirectory
     files = repo.get_contents(directory.path, ref="main")
-
     markdown_files = [file for file in files if file.name.endswith(".md")]
-    # Determine the week number from the directory name
-    week_match = re.match(r'^(\d+)_\w+$', directory.name)
 
+    # Determine the week number from the directory name
+    week_match = re.match(r'^week(\d+)$', directory.name)
     if week_match:
         week_number = int(week_match.group(1))
-    else:
-        continue  # Skip directories with invalid names
-
-    # Initialize a list to store summaries for this week
-    week_summaries = []
-
-    # Iterate through each markdown file in the subdirectory
-    for file in markdown_files:
-        content = file.decoded_content.decode("utf-8")
         
-        # Extract the summary content after "summary:"
-        summary_marker = "summary:"
-        summary_start = content.find(summary_marker)
-        if summary_start != -1:
-            summary_content = content[summary_start + len(summary_marker):].strip()
-            week_summaries.append(summary_content)
+        # Initialize a list to store summaries for this week
+        week_summaries = []
 
-    # Combine the extracted summaries for this week into a single string
-    combined_summary = '\n\n'.join(week_summaries)
+        # Iterate through each markdown file in the subdirectory
+        for file in markdown_files:
+            content = file.decoded_content.decode("utf-8")
 
-    # Define the filename for the week's summary
-    summary_filename = f"{week_number}_subteam.md"
-    summary_path = os.path.join(folder_path, summary_filename)
+            # Extract the summary content after "summary:"
+            summary_marker = "summary:"
+            summary_start = content.find(summary_marker)
+            if summary_start != -1:
+                summary_content = content[summary_start + len(summary_marker):].strip()
+                week_summaries.append(summary_content)
 
-    # Create or update the summary file
-    repo.update_file(summary_path, f"Update summary for week {week_number}", combined_summary, branch="main")
+        # Combine the extracted summaries for this week into a single string
+        combined_summary = '\n\n'.join(week_summaries)
 
-    # Store the combined summary in the dictionary
-    summaries_by_week[week_number] = combined_summary
+        # Check if this week is the latest week
+        if week_number > latest_week_number:
+            latest_week_number = week_number
+            latest_week_summary = combined_summary
 
-    # Append this week's summary to the combined main readme summary
-    combined_main_readme_summary += f"\n## Week {week_number} Summary\n\n{combined_summary}"
-print(combined_main_readme_summary)
-"""
+# At this point, you have the summary for the latest week in the variable `latest_week_summary`
+new_main_readme_summary = (f"# Latest Week Summary (Week {latest_week_number}):\n{latest_week_summary}")
+
 # Get the existing content of the main readme.md file
-readme_file = repo.get_contents("readme.md", ref="main")
-existing_readme_content = readme_file.decoded_content.decode("utf-8")
+readme_file = repo.get_contents("tutorial.md", ref="main")
+tutorial_readme = readme_file.decoded_content.decode("utf-8")
 
 # Combine the existing content and the new summaries
-updated_readme_content = existing_readme_content + combined_main_readme_summary
+updated_readme_content = new_main_readme_summary + tutorial_readme
 
 # Update the main readme.md file with the combined summaries
-repo.update_file("readme.md", "Update the README with the latest weekly update", updated_readme_content, readme_file.sha, branch="main")
-"""
+repo.update_file("README.md", "Update the README with the latest weekly update", updated_readme_content, readme_file.sha, branch="main")

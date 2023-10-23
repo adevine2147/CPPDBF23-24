fdf1e53aa92b309ea2bed40110d44eeabecb2c8a
AndrewLikeMango <adevine2147@gmail.com>
2023-10-23 13:31:29 -0700

    added simunlink files for takeoff analysis

---
+++ systems/Takeoff_Analysis.m
@@ -0,0 +1,15 @@
+%takeoff distance for conventional vs tandem
+%vary C_L, what else ?
+P = 64; %N
+m = 6.35; %kg
+C_L = 1.4; 
+C_do = .03; 
+mu = 0.015; 
+S_cref = 0.46; %m^2 5x1ft wing
+S_tref = 0.6 %m^2 two wings diff AR
+rho = 1.225; %kg/m^3
+g = 9.8; %m/s^2
+A_txsec = .0049*1.584 + .0166*1.524 + .014; %m^2 tandem 
+A_cxsec = .0166*1.524 + .014; %m^2 conventional 
+
+
